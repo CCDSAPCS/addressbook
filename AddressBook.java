44376491ea6b5d2a15c8eb96b549e15c4f830873
mtwyford <marcus.twyford@gmail.com>
2017-03-14 10:26:23 -0400

    Started new file in address book

---
+++ AddressBook.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class AddressBook {
+
+    private Contact[] friends; // An array of Contacts - each stores info for one friend
+    private int numfriends; // Number of friends currently in AddressBook
+
+    // Create an empty AddressBook
+    public AddressBook() {
+		friends = new Contact[10];
+		numfriends = 0;
+    }
