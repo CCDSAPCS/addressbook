70a81b15db96c90aab58948398f251530eda3ae0
mtwyford <mtwyford@users.noreply.github.com>
2017-03-16 09:24:09 -0400

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -12,3 +12,46 @@ public class Contact {
 		phonenumber = p;
 		bday = 100*month + day;
     }
+
+    // Changes phone number of contact
+    public void changeNumber(int newnum) {
+		phonenumber = newnum;
+    }
+
+    // Implements the passing of Contact's birthday.
+    public void Birthday() {
+		age++;
+    }
+
+    // Returns the name of a Contact
+    public String getName() {
+		return name;
+    }
+
+    // Returns the age of a Contact
+    public int getAge() {
+		return age;
+    }
+
+    // Returns the phone number of a Contact
+    public int getNumber() {
+		return phonenumber;
+    }
+
+    // Prints all information about a contact out.
+    public void printContact() {
+		System.out.print("Name: " + name + " Age: " + age + " Phone#: " + phonenumber);
+		System.out.println(" Birthday: " + getBdayMonth() + "/" + getBdayDay());
+    }
+
+    // Returns month of Contact's birthday
+    public int getBdayMonth() {
+		return bday/100;
+    }
+    
+    // Returns day of the month of Contact's birthday
+    public int getBdayDay() {
+		return bday%100;
+    }
+
+}
